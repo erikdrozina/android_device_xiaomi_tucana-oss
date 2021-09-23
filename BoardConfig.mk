f3f3e2d5b2319f0c9e51110dadebf1a7e48f1d5c
Erik Drozina <erikdro41@gmail.com>
2021-09-23 12:13:15 +0200

    Dropped permissive
    Signed-off-by: Erik Drozina <erikdro41@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -35,7 +35,7 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_tucana
 BOARD_BOOTIMG_HEADER_VERSION := 2
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 service_locator.enable=1 swiotlb=1 earlycon=msm_geni_serial,0x880000 loop.max_part=7 cgroup.memory=nokmem,nosocket kpti=off
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+# BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
